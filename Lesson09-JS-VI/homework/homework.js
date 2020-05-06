94645ceaf59b8684ef8074688a86cdb6f3618ea4
Crisca-cyver <63578755+Crisca-cyver@users.noreply.github.com>
2020-05-06 17:15:00 -0300

    update JSVI

--- Lesson09-JS-VI/homework/homework.js
+++ Lesson09-JS-VI/homework/homework.js
@@ -9,7 +9,7 @@ function sumarArray(numeros, cb) {
   // Suma todos los números enteros (int/integers) de un array ("numeros")
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
-va suma = numeros.reduce(function(acc,elemento) {
+var suma = numeros.reduce(function(acc,elemento) {
   return acc + elemento;
 }, 0);
 cb(suma);
@@ -18,7 +18,7 @@ cb(suma);
 function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
-
+array.forEach((e) => cb(e));
 
 }
 
@@ -26,6 +26,10 @@ function map(array, cb) {
   // Crea un nuevo array
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
+var nuevoArray = array.map(function(e) {
+  return cb(e);
+})
+return nuevoArray;
 }
 
 // No modificar nada debajo de esta línea
